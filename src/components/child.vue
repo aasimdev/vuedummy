6a0fdde59675522b9713937fa41fb5e5d63b10ca
Board Developer3 <board.developer3@outlook.com>
2023-01-26 22:23:03 +0500

    date 26/1

---
+++ src/components/child.vue
@@ -0,0 +1,22 @@
+<template>
+
+<h2>{{name}}</h2>
+<h2>
+    {{ user.name }}
+</h2>
+<button v-on:click="getData()">press btn</button>
+
+</template>
+
+<script>
+export default{
+
+    name : 'child',
+    props : {
+        name : String,
+        user : Object,
+        getData: Function
+    }
+
+}
+</script>
--- src/components/home.vue
+++ src/components/home.vue
@@ -17,9 +17,9 @@
     <button v-on:click="getValue()" type="submit">Get data</button> -->
 
 
-   
-    
-    <!----------------------3-------------------------------->
+
+
+    <!-- --------------------3------------------------------
     
     <label for="checkbox1">checbox1</label>
     <input type="checkbox" value="checbox1" id="checkbox1" v-model="checkbox">
@@ -30,11 +30,61 @@
     <label for="checkbox3">checbox3</label>
     <input type="checkbox" value="checbox3" id="checkbox3" v-model="checkbox">
     
-    <h4>Selected chexbox : {{ checkbox }}</h4>
+    <h4>Selected chexbox : {{ checkbox }}</h4> -->
+
+
+
+
+    <!----------------------4------------------------------>
+
+    <!-- loops in vue -->
+    <!-- <ul>
+        <li v-for="item in languages" :key="item">
+            {{ item }}
+        </li>
+    </ul>
+    <br>
+    <br>
+    <ul>
+        <li v-for="item in user " :key="item.email">
+            name: is {{ item.name }} email: is {{ item.email }}
+        </li>
+    </ul> -->
+
+
+
+    <!----------------------5------------------------------>
+
+    <!-- get data from child components -->
+
+    <!-- <child name="userName" :user="user" :getData="getData"/> -->
+
+    <!---------------------------6-------------------------------->
+    <!-- html binding -->
+    <!-- 
+     <div v-html="tag"></div>
+     <div v-html="tag + tag2 + tag3"></div> -->
+
+
+
+     
+    <!---------------------------7-------------------------------->
+    <!-- css binding -->
+
+    <h2 class="heading" :class="{active:applyclass}">
+        Home Components
+    </h2>
+    <button v-on:click="applyclass=!applyclass">Apply Class</button>
+
 </template>
 <script>
+
+import child from './child.vue';
 export default {
     name: 'home',
+    components: {
+        child
+    },
     // <!----------------------1-------------------------------->
     // data() {
     //     return {
@@ -65,17 +115,91 @@ export default {
     //         console.log('Value :',this.email,this.password)
     //      }
     // }
-    
+
 
 
 
     // <!----------------------3-------------------------------->
 
-    data() {
+    // data() {
+    //     return{
+    //         checkbox:[]
+    //     }
+    // }
+
+
+    // <!----------------------4-------------------------------->
+    //                     Loops in vue
+
+
+    // data() {
+    //     return {
+    //         languages: ['javascript', 'node', 'php', 'python',],
+    //         user: [{
+    //             name: 'umar',
+    //             email: 'mail@mail.com',
+    //         },
+    //         {
+    //             name: 'abdullah',
+    //             email: 'mail@mail.com',
+    //         },
+    //         {
+    //             name: 'rehman',
+    //             email: 'mail@mail.com',
+    //         },
+    //         {
+    //             name: 'rohan',
+    //             email: 'mail@mail.com',
+    //         }]
+    //     }
+    // }
+
+
+
+
+
+    // <!----------------------5-------------------------------->
+    // //               get data from child components
+
+    // data() {
+    //     return {
+    //         user:{name: 'peter',email: 'peter@mail.com'}
+    //     }
+    // },
+    // methods:{
+    //     getData(){
+    //         console.warn("function from home component")
+    //     }
+    // }
+
+
+
+    // <!----------------------6-------------------------------->
+    //  html binding
+
+
+    // data() {
+    //     return {
+    //         tag: '<h1>data</h1>',
+    //         tag2: '<h2>data</h2>',
+    //         tag3: '<h3>data</h3>',
+    //     }
+    // }
+
+
+    // <!---------------------------7--------------------------------->
+    // css binding
+
+
+    data(){
         return{
-            checkbox:[]
+            applyclass: false
         }
-    }
- 
+    },
 }
 </script>
+<style>
+.active{
+    color: blue;
+}
+</style>
